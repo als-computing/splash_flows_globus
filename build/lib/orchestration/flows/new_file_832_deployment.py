ac1f2d846b4669f502bda1ebd9699a0edd8c0239
dylanmcreynolds <dmcreynolds@lbl.gov>
2023-01-30 17:39:10 +0000

    add gitignore clean bad dirs

--- build/lib/orchestration/flows/new_file_832_deployment.py
+++
@@ -1,8 +0,0 @@
-from prefect.deployments import Deployment
-
-Deployment(
-    name="832",
-    flow="flows/new_file_832.py",
-    tags=["orchestration", "832"],
-    parameters={"file_path": "test"},
-)
