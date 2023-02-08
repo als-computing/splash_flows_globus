9899aca69c7e5bf724f508b12c0416895b415f21
Dylan McReynolds <dmcreynolds@lbl.gov>
2023-02-07 17:40:57 -0800

    fixed setup.py name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="splash_auth",
+    name="splash_flows_globus",
     version="1.0",
     description="Prefect-based workflow code for moving data with Globus",
     author="dylan mcreynolds",
