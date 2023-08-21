d3c94dfb4734a864d7a268eab475aa74c2dc94fb
Dylan McReynolds <dmcreynolds@lbl.gov>
2023-08-21 09:08:04 -0700

    move ptycho_jobscript.py

--- orchestration/nersc.py
+++ orchestration/nersc.py
@@ -1,13 +1,13 @@
 from authlib.integrations.requests_client import OAuth2Session
 from authlib.oauth2.rfc7523 import PrivateKeyJWT
 import time, json, os, logging
-from orchestration.ptycho_jobscript import (
-    get_job_script,
-    cdtool_args_string,
-    ptychocam_args_string,
-    cdtools_parms,
-    ptychocam_parms,
-)
+# from orchestration.ptycho_jobscript import (
+#     get_job_script,
+#     cdtool_args_string,
+#     ptychocam_args_string,
+#     cdtools_parms,
+#     ptychocam_parms,
+# )
 
 
 class NerscClient:
@@ -110,28 +110,28 @@ class NerscClient:
         self.update_task_id()
         self.logger.info(f"Submitted task id: {self.task_id}")
 
-    def cdtools(self, cxiname, path_job_script, path_cdtools_nersc, n_gpu, **kwargs):
-        args_string = cdtool_args_string(
-            cxiname, path_cdtools_nersc, cdtools_parms, **kwargs
-        )
-        job_script = get_job_script(path_job_script, n_gpu, args_string)
-        self.logger.info(f"Job script: {job_script}")
+    # def cdtools(self, cxiname, path_job_script, path_cdtools_nersc, n_gpu, **kwargs):
+    #     args_string = cdtool_args_string(
+    #         cxiname, path_cdtools_nersc, cdtools_parms, **kwargs
+    #     )
+    #     job_script = get_job_script(path_job_script, n_gpu, args_string)
+    #     self.logger.info(f"Job script: {job_script}")
 
-        self.submit_job(job_script)
-        self.task_wait()
+    #     self.submit_job(job_script)
+    #     self.task_wait()
 
-    def ptychocam(
-        self, cxiname, path_job_script, path_ptychocam_nersc, n_gpu, **kwargs
-    ):
-        args_string = ptychocam_args_string(
-            cxiname, path_ptychocam_nersc, ptychocam_parms, **kwargs
-        )
-        job_script = get_job_script(path_job_script, n_gpu, args_string)
+    # def ptychocam(
+    #     self, cxiname, path_job_script, path_ptychocam_nersc, n_gpu, **kwargs
+    # ):
+    #     args_string = ptychocam_args_string(
+    #         cxiname, path_ptychocam_nersc, ptychocam_parms, **kwargs
+    #     )
+    #     job_script = get_job_script(path_job_script, n_gpu, args_string)
 
-        self.logger.info(f"Job script: {job_script}")
+    #     self.logger.info(f"Job script: {job_script}")
 
-        self.submit_job(job_script)
-        self.task_wait()
+    #     self.submit_job(job_script)
+    #     self.task_wait()
 
     def task_wait(self, max_wait_seconds=600, sleep_time=2):
         start = time.time()
