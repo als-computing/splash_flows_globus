dcab378acb114a2f366f27ba4906dec11192a7f6
Dylan McReynolds <dmcreynolds@lbl.gov>
2023-10-09 17:00:20 -0700

    add logging to ingest

--- orchestration/flows/bl832/move.py
+++ orchestration/flows/bl832/move.py
@@ -118,6 +118,7 @@ def process_new_832_file(file_path: str, is_export_control=False, send_to_nersc=
             f"File successfully transferred from data832 to NERSC {file_path}. Task {task}"
         )
         flow_name = f"ingest scicat: {Path(file_path).name}"
+        logger.info(f"Ingesting {file_path} with {TOMO_INGESTOR_MODULE}")
         try:
             ingest_dataset(file_path, TOMO_INGESTOR_MODULE)
         except Exception as e:
