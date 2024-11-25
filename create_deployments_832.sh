fd99b558fac8596bc6f43efb868a2c3453b8ab9a
David Abramov <dabramov@lbl.gov>
2024-11-25 12:01:32 -0800

    Updated Prefect deployments for new_file_832. Created three new work pools: 'new_file_832_flow_pool' (2 queues), 'new_file_832_prune_pool' (2 queues), and 'scicat_ingest_pool' (1 queue).

--- create_deployments_832.sh
+++ create_deployments_832.sh
@@ -1,21 +1,33 @@
 export $(grep -v '^#' .env | xargs)
 
 
-prefect deployment build ./orchestration/flows/bl832/move.py:process_new_832_file -n new_file_832 -p default-agent-pool -q bl832_main_queue
+# Create work pools. If a work pool already exists, it will throw a warning but that's no problem
+prefect work-pool create 'new_file_832_flow_pool'
+prefect work-pool create 'new_file_832_prune_pool'
+prefect work-pool create 'scicat_ingest_pool'
+
+# new_file_832_flow_pool
+    # in docker-compose.yaml:
+    # command: prefect agent start --pool "new_file_832_flow_pool"
+prefect deployment build ./orchestration/flows/bl832/move.py:process_new_832_file -n new_file_832 -p new_file_832_flow_pool -q new_file_832_queue
 prefect deployment apply process_new_832_file-deployment.yaml
 
-
-prefect deployment build ./orchestration/flows/bl832/move.py:test_transfers_832 -n test_transfers_832 -p default-agent-pool -q bl832_test_queue
+prefect deployment build ./orchestration/flows/bl832/move.py:test_transfers_832 -n test_transfers_832 -p new_file_832_flow_pool -q test_transfers_832_queue
 prefect deployment apply test_transfers_832-deployment.yaml
 
+# new_file_832_prune_pool
+    # in docker-compose.yaml:
+    # command: prefect agent start --pool "new_file_832_prune_pool"
 
-prefect deployment build ./orchestration/flows/bl832/prune.py:prune_spot832 -n prune_spot832 -p default-agent-pool -q bl832_prune_queue
+prefect deployment build ./orchestration/flows/bl832/prune.py:prune_spot832 -n prune_spot832 -p new_file_832_prune_pool -q prune_spot832_queue
 prefect deployment apply prune_spot832-deployment.yaml
 
-
-prefect deployment build ./orchestration/flows/bl832/prune.py:prune_data832 -n prune_data832 -p default-agent-pool -q bl832_prune_queue
+prefect deployment build ./orchestration/flows/bl832/prune.py:prune_data832 -n prune_data832 -p new_file_832_prune_pool -q prune_data832_queue
 prefect deployment apply prune_data832-deployment.yaml
 
+# scicat_ingest_pool
+    # in docker-compose.yaml:
+    # command: prefect agent start --pool "scicat_ingest_pool"
 
-prefect deployment build ./orchestration/flows/scicat/ingest.py:ingest_dataset -n ingest_dataset -p default-agent-pool -q bl832_ingest_queue
+prefect deployment build ./orchestration/flows/scicat/ingest.py:ingest_dataset -n ingest_dataset -p scicat_ingest_pool
 prefect deployment apply ingest_dataset-deployment.yaml
