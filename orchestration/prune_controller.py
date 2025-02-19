fb9823071cc4c3db46b50ef12096cf3a68e64f7f
David Abramov <dabramov@lbl.gov>
2025-02-19 09:56:04 -0800

    Moving specific prune implementations as private/internal methods in their respective PruneControllers, with Prefect Flow decorators.

--- orchestration/prune_controller.py
+++ orchestration/prune_controller.py
@@ -82,6 +82,24 @@ class HPSSPruneController(PruneController[HPSSEndpoint]):
         )
         return True
 
+    @flow(name="prune_hpss_endpoint")
+    def _prune_hpss_endpoint(
+        relative_path: str,
+        source_endpoint: HPSSEndpoint,
+        check_endpoint: Union[FileSystemEndpoint, None] = None,
+        config: BeamlineConfig = None
+    ):
+        """
+        Prune files from HPSS.
+
+        Args:
+            relative_path (str): The path of the file or directory to prune.
+            source_endpoint (HPSSEndpoint): The Globus source endpoint to prune from.
+            check_endpoint (FileSystemEndpoint, optional): The Globus target endpoint to check. Defaults to None.
+        """
+        # TODO: Implement HPSS pruning
+        pass
+
 
 class FileSystemPruneController(PruneController[FileSystemEndpoint]):
     def __init__(
@@ -113,6 +131,46 @@ class FileSystemPruneController(PruneController[FileSystemEndpoint]):
             datetime.timedelta(days=days_from_now),
         )
         return True
+
+    @flow(name="prune_filesystem_endpoint")
+    def _prune_filesystem_endpoint(
+        relative_path: str,
+        source_endpoint: FileSystemEndpoint,
+        check_endpoint: Union[FileSystemEndpoint, None] = None,
+        config: BeamlineConfig = None
+    ):
+        """
+        Prune files from a File System.
+
+        Args:
+            relative_path (str): The path of the file or directory to prune.
+            source_endpoint (FileSystemEndpoint): The Globus source endpoint to prune from.
+            check_endpoint (FileSystemEndpoint, optional): The Globus target endpoint to check. Defaults to None.
+        """
+        logger.info(f"Running flow: prune_from_{source_endpoint.name}")
+        logger.info(f"Pruning {relative_path} from source endpoint: {source_endpoint.name}")
+
+        # Check if the file exists at the source endpoint
+        if not source_endpoint.exists(relative_path):
+            logger.warning(f"File {relative_path} does not exist at the source: {source_endpoint.name}.")
+            return
+
+        # Check if the file exists at the check endpoint
+        if check_endpoint is not None and check_endpoint.exists(relative_path):
+            logger.info(f"File {relative_path} exists on the check point: {check_endpoint.name}.")
+            logger.info("Safe to prune.")
+
+            # Check if it is a file or directory
+            if source_endpoint.is_dir(relative_path):
+                logger.info(f"Pruning directory {relative_path}")
+                source_endpoint.rmdir(relative_path)
+            else:
+                logger.info(f"Pruning file {relative_path}")
+                os.remove(source_endpoint.full_path(relative_path))
+        else:
+            logger.warning(f"File {relative_path} does not exist at the check point: {check_endpoint.name}.")
+            logger.warning("Not safe to prune.")
+        return
 
 
 class GlobusPruneController(PruneController[GlobusEndpoint]):
@@ -149,6 +207,36 @@ class GlobusPruneController(PruneController[GlobusEndpoint]):
         )
         return True
 
+    @flow(name="prune_globus_endpoint")
+    def _prune_globus_endpoint(
+        relative_path: str,
+        source_endpoint: GlobusEndpoint,
+        check_endpoint: Union[GlobusEndpoint, None] = None,
+        config: BeamlineConfig = None
+    ) -> None:
+        """
+        Prune files from a Globus endpoint.
+
+        Args:
+            relative_path (str): The path of the file or directory to prune.
+            source_endpoint (GlobusEndpoint): The Globus source endpoint to prune from.
+            check_endpoint (GlobusEndpoint, optional): The Globus target endpoint to check. Defaults to None.
+        """
+        globus_settings = JSON.load("globus-settings").value
+        max_wait_seconds = globus_settings["max_wait_seconds"]
+        flow_name = f"prune_from_{source_endpoint.name}"
+        logger.info(f"Running flow: {flow_name}")
+        logger.info(f"Pruning {relative_path} from source endpoint: {source_endpoint.name}")
+        prune_one_safe(
+            file=relative_path,
+            if_older_than_days=0,
+            tranfer_client=config.tc,
+            source_endpoint=source_endpoint,
+            check_endpoint=check_endpoint,
+            logger=logger,
+            max_wait_seconds=max_wait_seconds
+        )
+
 
 def get_prune_controller(
     endpoint: TransferEndpoint,
@@ -162,93 +250,3 @@ def get_prune_controller(
         return GlobusPruneController(config)
     else:
         raise ValueError(f"Unsupported endpoint type: {type(endpoint)}")
-
-
-@flow(name="prune_globus_endpoint")
-def prune_globus_files(
-    relative_path: str,
-    source_endpoint: GlobusEndpoint,
-    check_endpoint: Union[GlobusEndpoint, None] = None,
-    config: BeamlineConfig = None
-):
-    """
-    Prune files from a source endpoint.
-
-    Args:
-        relative_path (str): The path of the file or directory to prune.
-        source_endpoint (GlobusEndpoint): The Globus source endpoint to prune from.
-        check_endpoint (GlobusEndpoint, optional): The Globus target endpoint to check. Defaults to None.
-    """
-    globus_settings = JSON.load("globus-settings").value
-    max_wait_seconds = globus_settings["max_wait_seconds"]
-    flow_name = f"prune_from_{source_endpoint.name}"
-    logger.info(f"Running flow: {flow_name}")
-    logger.info(f"Pruning {relative_path} from source endpoint: {source_endpoint.name}")
-    prune_one_safe(
-        file=relative_path,
-        if_older_than_days=0,
-        tranfer_client=config.tc,
-        source_endpoint=source_endpoint,
-        check_endpoint=check_endpoint,
-        logger=logger,
-        max_wait_seconds=max_wait_seconds
-    )
-
-
-@flow(name="prune_filesystem_endpoint")
-def prune_filesystem_files(
-    relative_path: str,
-    source_endpoint: FileSystemEndpoint,
-    check_endpoint: Union[FileSystemEndpoint, None] = None,
-    config: BeamlineConfig = None
-):
-    """
-    Prune files from a source endpoint.
-
-    Args:
-        relative_path (str): The path of the file or directory to prune.
-        source_endpoint (FileSystemEndpoint): The Globus source endpoint to prune from.
-        check_endpoint (FileSystemEndpoint, optional): The Globus target endpoint to check. Defaults to None.
-    """
-    logger.info(f"Running flow: prune_from_{source_endpoint.name}")
-    logger.info(f"Pruning {relative_path} from source endpoint: {source_endpoint.name}")
-
-    # Check if the file exists at the source endpoint
-    if not source_endpoint.exists(relative_path):
-        logger.warning(f"File {relative_path} does not exist at the source: {source_endpoint.name}.")
-        return
-
-    # Check if the file exists at the check endpoint
-    if check_endpoint is not None and check_endpoint.exists(relative_path):
-        logger.info(f"File {relative_path} exists on the check point: {check_endpoint.name}.")
-        logger.info("Safe to prune.")
-
-        # Check if it is a file or directory
-        if source_endpoint.is_dir(relative_path):
-            logger.info(f"Pruning directory {relative_path}")
-            source_endpoint.rmdir(relative_path)
-        else:
-            logger.info(f"Pruning file {relative_path}")
-            os.remove(source_endpoint.full_path(relative_path))
-    else:
-        logger.warning(f"File {relative_path} does not exist at the check point: {check_endpoint.name}.")
-        logger.warning("Not safe to prune.")
-    return
-
-
-@flow(name="prune_hpss_endpoint")
-def prune_hpss_files(
-    relative_path: str,
-    source_endpoint: HPSSEndpoint,
-    check_endpoint: Union[FileSystemEndpoint, None] = None,
-    config: BeamlineConfig = None
-):
-    """
-    Prune files from a source endpoint.
-
-    Args:
-        relative_path (str): The path of the file or directory to prune.
-        source_endpoint (GlobusEndpoint): The Globus source endpoint to prune from.
-        check_endpoint (GlobusEndpoint, optional): The Globus target endpoint to check. Defaults to None.
-    """
-    pass
