30da0a7dd6e80ef2af0194c486c44a16c7a57ffb
swelborn <55868530+swelborn@users.noreply.github.com>
2025-03-10 22:19:31 -0400

    dark theme

--- frontend/streaming-frontend/src/App.tsx
+++ frontend/streaming-frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { FlowDashboard } from './components/FlowDashboard'
 import './App.css'
+import { ThemeProvider } from './components/ThemeProvider'
 
 const queryClient = new QueryClient()
 
 function AppWithProvider() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <FlowDashboard />
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <QueryClientProvider client={queryClient}>
+        <FlowDashboard />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
